732da6b7050dbfbbcd655248e29c561e2cce81f2
afrizal0811 <afrizalmaulana0811@gmail.com>
2023-12-19 16:34:20 +0700

    edit dashboard

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
 import Link from 'next/link'
-
+import DashboardButton from './components/DashboardButton'
 export default function Home() {
   return (
     <>
-      <h1>Hai</h1>
-      <Link href='/pages/login'>Login</Link>
-      <br/>
-      <Link href='/pages/register'>Register</Link>
+      <DashboardButton />
+
     </>
   )
 }
